132b98874bc801fc51c2408f999ca461f88c6742
Annette Barbara Wysmolek <annette.wysmolek@stud.hn.de>
2024-06-23 15:34:17 +0200

    Netzwerk und Matrix Nachricht senden

--- pico_client.py
+++ pico_client.py
@@ -1,1 +1,24 @@
+# Project objective: Blink the onboard LED of the Raspberry Pi Pico W
+# Programmer: Adrian Josele G. Quional
 
+# modules
+from machine import Pin # to be able to work with the hardware
+from time import sleep	# to add delays in the program
+
+# Setting the pin mode to OUT
+# Pin 25 (GP25) is the onboard LED in Raspberry Pi Pico W
+# for Raspberry Pi Pico
+# LED_PIN = Pin(25, Pin.OUT)
+
+# for Raspberry Pi Pico W
+LED = Pin("LED", Pin.OUT)
+
+# continuously blink the onboard LED for an interval of 1 sec while the board has power
+while True:
+    LED.on()	# turn on LED
+    print ("ON")
+    sleep(1)	# wait for 1 sec
+    LED.off()	# turn off LED
+    print ("off")
+    sleep(1)	# wait for 1 sec
+    
