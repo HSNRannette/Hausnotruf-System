477aea26f9b012cdcf1137f72debd1f3aa80d35a
Annette Barbara Wysmolek <annette.wysmolek@stud.hn.de>
2024-06-24 13:21:54 +0200

    update

--- Code-Hausnotruf-Knopf/main_RPI4oo.py
+++ Code-Hausnotruf-Knopf/main_RPI4oo.py
@@ -1,84 +1,111 @@
+import network
+import urequests as requests
 import time
-import requests
-import RPi.GPIO as GPIO
-from gpiozero import PWMLED, Button
-from smbus2 import SMBus
-from mpu6050 import mpu6050
+from machine import Pin, PWM
 
-# Netzwerk einrichten WLAN-Verbindung
-ssid = AndroidAP25A5
-password = gjht7545
+# Netzwerk einrichten/ WLAN-Verbindung
+ssid = "AndroidAP25A5"
+password = "gjht7545"
 
-# Das Netzwerk wird in der Regel über die grafische Oberfläche oder ein Terminal eingerichtet
-# Siehe vorherige Anleitung
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
 
 # RGB-LED Setup
-led_r = PWMLED(15)
-led_g = PWMLED(16)
-led_b = PWMLED(17)
+led_r = PWM(Pin(15))
+led_g = PWM(Pin(16))
+led_b = PWM(Pin(17))
 
-def set_led_color(r, g, b)
-    led_r.value = r  255.0
-    led_g.value = g  255.0
-    led_b.value = b  255.0
+led_r.freq(1000)
+led_g.freq(1000)
+led_b.freq(1000)
 
-def blink_led(r, g, b, duration=0.5)
+def set_led_color(r, g, b):
+    led_r.duty_u16(int(r * 65535 / 255))
+    led_g.duty_u16(int(g * 65535 / 255))
+    led_b.duty_u16(int(b * 65535 / 255))
+
+def blink_led(r, g, b, duration=0.5):
     set_led_color(r, g, b)
     time.sleep(duration)
     set_led_color(0, 0, 0)
     time.sleep(duration)
 
+# Blinkt Blau, während die Verbindung hergestellt wird
+while not wlan.isconnected():
+    print("Verbinden...")
+    blink_led(0, 0, 255, duration=0.5)
+
+print("Verbunden, IP Adresse:", wlan.ifconfig()[0])
+
 # Matrix-API Einstellungen
-matrix_server = httpsmatrix.org  # Matrix-Server, Element als App installiert
-access_token = syt_aHNucmFuYmE_vSvbFHbibprhTxvJBBoD_4IRejw  # Teilnehmerin
-room_id = !FTmUTmQleJsspLegXDmatrix.org  
-# oder der Raum auf deinem Server httpsmatrix.to##NotrufePicoWmatrix.org
+matrix_server = "https://matrix.org"
+access_token = "syt_aHNucmFuYmE_vSvbFHbibprhTxvJBBoD_4IRejw"
+room_id = "!FTmUTmQleJsspLegXD:matrix.org"
 
 # Nachricht senden
-def send_message(message)
-    url = f{matrix_server}_matrixclientr0rooms{room_id}sendm.room.messageaccess_token={access_token}
+def send_message(message):
+    url = f"{matrix_server}/_matrix/client/r0/rooms/{room_id}/send/m.room.message?access_token={access_token}"
     headers = {
-        Content-Type applicationjson
+        "Content-Type": "application/json"
     }
     payload = {
-        msgtype m.text,
-        body message
+        "msgtype": "m.text",
+        "body": message
     }
     
     response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200
-        print(Nachricht gesendet System gestartet und verbunden!)
-    else
-        print(fFehler beim Senden der Nachricht {response.status_code}, {response.text})
+    if response.status_code == 200:
+        print("Nachricht gesendet: System gestartet und verbunden!")
+    else:
+        print(f"Fehler beim Senden der Nachricht: {response.status_code}, {response.text}")
 
 # Nachricht senden
-send_message(System gestartet und verbunden)
+send_message("System gestartet und verbunden")
 set_led_color(0, 255, 0)  # Grün ohne Blinken
 
 # Setup für Notfallknopf
-button = Button(14)
-print(Notfallknopf aktiviert)
+button = Pin(14, Pin.IN, Pin.PULL_UP)
+print("Notfallknopf aktiviert")
 
-# Setup für den Beschleunigungssensor MPU6050
-i2c = SMBus(1)
-accelerometer = mpu6050(0x68)
-
-# Funktion zum Erkennen eines Sturzes
-def detect_fall()
-    threshold = 15
-    acceleration = accelerometer.get_accel_data()
-    total_acc = sum([abs(acceleration['x']), abs(acceleration['y']), abs(acceleration['z'])])
-    return total_acc  threshold
-
-def wait_for_button_press(duration)
+def wait_for_button_press(duration):
     start_time = time.time()
-    while time.time() - start_time  duration
-        if button.is_pressed
+    while time.time() - start_time < duration:
+        if button.value() == 0:
             return False
         time.sleep(0.1)
     return True
 
-try
-    while True
-        if button.is_pressed
-            set_led_color(0
+try:
+    while True:
+        if button.value() == 0:
+            set_led_color(255, 0, 0)  # Rot
+            send_message("Hilferuf, Patient A!")
+            print("Hilfe, Notfallknopf gedrückt!")
+            for _ in range(20):  # Blinkt 20 mal
+                set_led_color(255, 0, 0)
+                time.sleep(0.1)
+                set_led_color(0, 0, 0)
+                time.sleep(0.1)
+        
+            # Blink LED rot bis der Knopf für 5 Sekunden gedrückt wird
+            while True:
+                blink_led(255, 0, 0, duration=0.5)
+                if button.value() == 0:
+                    if wait_for_button_press(5):
+                        break
+            
+            # Nach 5 Sekunden drücken wechselt die LED zu Grün und Schleife ist wieder aktiv
+            set_led_color(0, 255, 0)  # Grün ohne Blinken
+            print("Pflegepersonal kommt!")
+            send_message("Patient A erhielt Hilfe")
+            
+        time.sleep(0.1)
+finally:
+    # GPIO-Pins freigeben
+    led_r.deinit()
+    led_g.deinit()
+    led_b.deinit()
+    button.init(Pin.IN)  # Setzt den Knopf-Pin auf den Standard-Eingangsmodus
+    wlan.active(False)  # WLAN deaktivieren
+    print("GPIOs freigegeben und WLAN deaktiviert")
